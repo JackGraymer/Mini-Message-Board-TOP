2d33d6ecabae56955e802885ce0aafb1e47c4c9c
Jack Graymer <jackgraymer@gmail.com>
2023-05-06 05:25:55 +0200

    Adds function to add new messages and structure them

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Mini Messageboard", messages: messages });
+  res.render("index", { title: "Latest messages", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+  //messages.push(req.body.name);
+  messages.unshift({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  console.log(messages);
+  res.redirect("/");
 });
 
 const messages = [
