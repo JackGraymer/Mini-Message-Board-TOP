4969a67cd5ea3b99b7f8e2ecb756d5cf7db8793c
Jack Graymer <jackgraymer@gmail.com>
2023-05-08 15:04:11 +0200

    cleanes code

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const router = express.Router();
 /* GET home page. */
 router.get("/", async function (req, res, next) {
   const messages = await Messages.find().sort({ date: -1 });
-  console.log(messages);
   res.render("index", { title: "Latest messages", messages: messages });
 });
 
@@ -19,6 +18,4 @@ router.post("/new", function (req, res, next) {
   res.redirect("/");
 });
 
-const allmessages = Messages.findOne();
-console.log(allmessages);
 module.exports = router;
