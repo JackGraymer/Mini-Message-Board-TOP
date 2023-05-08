6538953687549c579a7afa09fbc1ac7874b2dd1b
Jack Graymer <jackgraymer@gmail.com>
2023-05-08 14:56:24 +0200

    adds dotenv package, connects to mongoDB with mongoose

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+/* const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb+srv://jackgraymer:${process.env.PASSWORD}@cluster0.sohmtw9.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir); */
+
 var indexRouter = require("./routes/index");
 var newRouter = require("./routes/new");
 
 var app = express();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = `mongodb+srv://jackgraymer:${process.env.PASSWORD}@cluster0.sohmtw9.mongodb.net/?retryWrites=true&w=majority`;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log("Connected to MongoDB");
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
